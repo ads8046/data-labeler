760ca99007becb7f5f41ad5b3502d41c93e93346
Atharva Shivankar <ads8046@g.rit.edu>
2021-01-17 22:08:31 -0500

    Added the photo database (3 tables: Photos, User, User swipes) - 01/17

--- app/base.py
+++ app/base.py
@@ -1,7 +1,37 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 import requests
 
+import os
+
 app = Flask(__name__, template_folder='views/templates', static_folder='views/static')
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "photodatabase.db"))
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Photos(db.Model):
+    photo_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    photo_name = db.Column(db.String(255), nullable=False)
+    photo_date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    email_address = db.Column(db.String(100), nullable=False)
+
+
+class UserPhotoSwipes(db.Model):
+    photo_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    user_selection = db.Column(db.Boolean)
 
 
 @app.route('/')
